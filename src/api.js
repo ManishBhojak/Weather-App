e4203af6edc3a944721e1c0f144c6f4939c3c850
ManishBhojak <manishbhojakm8@gmail.com>
2023-01-03 20:58:26 +0530

    Weather-App-react

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'a96aa81264msh5ff39a97c24d957p1eb9c9jsn7be25aa89314',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "329343ed515e8704ed6da19b004fd128";
